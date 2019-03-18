421b4e46c98e1ec10fda9ff13eedbcaa9f0e8737
Sandor Martin Leyva <s.martin@estudiantes.matcom.uh.cu>
2019-03-18 06:44:56 +0100

    added woman and man to person.py

--- person.py
+++ person.py
@@ -1,13 +1,13 @@
 
 class Person:
-	def __init__(self, age, gender, max_children, birthday):
+	def __init__(self, age, gender, birthday_month):
 		self.age = age
 		self.gender = gender
 		self.civil_status = 0
 		self.time_out = 0
-		self.max_children = max_children
+		self.max_children = 0 # TODO se genera
 		self.children = 0
-		self.birthday = birthday
+		self.birthday_month = birthday_month
 		self.partner = None
 
 	# TODO
@@ -26,4 +26,25 @@ class Person:
 
 	def breaking_off(self):
 		# TODO generar a ver si se rompe la relacion
-		# en caso de que se rompa
+		# en caso de que se rompa quitar las referencias
+
+	def birthday(self, month):
+		self.age = self.age+1 if not (month-self.birthday_month)%12 else self.age
+
+
+class Woman(Person):
+	def __init__(self, age,birthday_month):
+		super().__init__(age,False, birthday_month)
+		self.giving_birth = 0
+
+	def pregnancy(self):
+		# TODO calcular a ver sin se embaraza
+		preg = True
+		if preg and self.partner is not None and self.children != self.max_children \
+				and self.partner.children != self.partner.max_children:
+			self.giving_birth = 9
+
+class Man(Person):
+	def __init__(self, age,birthday_month):
+		super().__init__(age,True, birthday_month)
+
